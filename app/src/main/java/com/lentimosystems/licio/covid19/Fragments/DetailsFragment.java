b03e255f6dd60a10cbe8866ea35fd2bb1f53b3c3
liciolentimo <lentimo@gmail.com>
2020-03-24 22:06:50 +0300

    added quiz navigation animation

--- app/src/main/java/com/lentimosystems/licio/covid19/Fragments/DetailsFragment.java
+++ app/src/main/java/com/lentimosystems/licio/covid19/Fragments/DetailsFragment.java
@@ -6,19 +6,40 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.lentimosystems.licio.covid19.Adapter.QuizListViewModel;
+import com.lentimosystems.licio.covid19.Model.QuizListModel;
 import com.lentimosystems.licio.covid19.R;
+
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class DetailsFragment extends Fragment {
+public class DetailsFragment extends Fragment implements View.OnClickListener {
+    private NavController navController;
+    private QuizListViewModel quizListViewModel;
+    private int position;
+    private ImageView detailsImage;
+    private TextView detailsTitle;
+    private TextView detailsDesc;
+    private TextView detailsDiff;
+    private TextView detailsQuestions;
 
+    private Button detailsStartBtn;
 
     public DetailsFragment() {
         // Required empty public constructor
@@ -36,7 +57,51 @@ public class DetailsFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        int position = DetailsFragmentArgs.fromBundle(getArguments()).getPosition();
+        navController = Navigation.findNavController(view);
+
+        //Initialize UI Elements
+        detailsImage = view.findViewById(R.id.details_image);
+        detailsTitle = view.findViewById(R.id.details_title);
+        detailsDesc = view.findViewById(R.id.details_desc);
+        detailsDiff = view.findViewById(R.id.details_difficulty_text);
+        detailsQuestions = view.findViewById(R.id.details_questions_text);
+
+        detailsStartBtn = view.findViewById(R.id.details_start_btn);
+        detailsStartBtn.setOnClickListener(this);
+
+         position = DetailsFragmentArgs.fromBundle(getArguments()).getPosition();
         Log.d("APP_LOG", "Position : " + position);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        quizListViewModel = new ViewModelProvider(getActivity()).get(QuizListViewModel.class);
+        quizListViewModel.getQuizListModelData().observe(getViewLifecycleOwner(), new Observer<List<QuizListModel>>() {
+            @Override
+            public void onChanged(List<QuizListModel> quizListModels) {
+                Glide.with(getContext())
+                        .load(quizListModels.get(position).getImage())
+                        .centerCrop()
+                        .placeholder(R.drawable.placeholder_image)
+                        .into(detailsImage);
+                detailsTitle.setText(quizListModels.get(position).getName());
+                detailsDesc.setText(quizListModels.get(position).getDesc());
+                detailsDiff.setText(quizListModels.get(position).getQuestionType());
+                detailsQuestions.setText(quizListModels.get(position).getQuestions() + "");
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.details_start_btn:
+                DetailsFragmentDirections.ActionDetailsFragmentToQuizFragment action = DetailsFragmentDirections.actionDetailsFragmentToQuizFragment();
+                action.setPosition(position);
+                navController.navigate(action);
+                break;
+        }
     }
 }
