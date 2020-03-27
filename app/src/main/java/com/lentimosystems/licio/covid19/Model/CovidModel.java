fc4e060ee379fd546fe023e1bf1701216d114fe0
liciolentimo <lentimo@gmail.com>
2020-03-27 10:59:41 +0300

    fixed error on model

--- app/src/main/java/com/lentimosystems/licio/covid19/Model/CovidModel.java
+++ app/src/main/java/com/lentimosystems/licio/covid19/Model/CovidModel.java
@@ -1,60 +1,166 @@
 package com.lentimosystems.licio.covid19.Model;
 
-public class CovidModel {
-    public String country;
-    public Integer cases;
-    public Integer todayCases;
-    public Integer deaths;
-    public Integer todayDeaths;
-    public Integer recovered;
-    public Integer critical;
-    public Integer casesPerOneMillion;
+import java.io.Serializable;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
 
+
+public class CovidModel implements Serializable {
+
+    @SerializedName("country")
+    @Expose
+    public String country;
+    @SerializedName("countryInfo")
+    @Expose
+    public CountryInfo countryInfo;
+    @SerializedName("cases")
+    @Expose
+    public Integer cases;
+    @SerializedName("todayCases")
+    @Expose
+    public Integer todayCases;
+    @SerializedName("deaths")
+    @Expose
+    public Integer deaths;
+    @SerializedName("todayDeaths")
+    @Expose
+    public Integer todayDeaths;
+    @SerializedName("recovered")
+    @Expose
+    public Integer recovered;
+    @SerializedName("active")
+    @Expose
+    public Integer active;
+    @SerializedName("critical")
+    @Expose
+    public Integer critical;
+    @SerializedName("casesPerOneMillion")
+    @Expose
+    public float casesPerOneMillion;
+    @SerializedName("deathsPerOneMillion")
+    @Expose
+    public float deathsPerOneMillion;
+    private final static long serialVersionUID = 7670782262320372395L;
+
+    /**
+     * No args constructor for use in serialization
+     */
     public CovidModel() {
     }
 
-    public CovidModel(String country, Integer totalCases, Integer todayCases, Integer totalDeaths, Integer todayDeaths, Integer recovered, Integer critical, Integer casesPerOneMillion) {
+    /**
+     * @param country
+     * @param recovered
+     * @param cases
+     * @param critical
+     * @param deathsPerOneMillion
+     * @param active
+     * @param casesPerOneMillion
+     * @param countryInfo
+     * @param deaths
+     * @param todayCases
+     * @param todayDeaths
+     */
+    public CovidModel(String country, CountryInfo countryInfo, Integer cases, Integer todayCases, Integer deaths, Integer todayDeaths, Integer recovered, Integer active, Integer critical, float casesPerOneMillion, float deathsPerOneMillion) {
+        super();
         this.country = country;
-        this.cases = totalCases;
+        this.countryInfo = countryInfo;
+        this.cases = cases;
         this.todayCases = todayCases;
-        this.deaths = totalDeaths;
+        this.deaths = deaths;
         this.todayDeaths = todayDeaths;
         this.recovered = recovered;
+        this.active = active;
         this.critical = critical;
         this.casesPerOneMillion = casesPerOneMillion;
+        this.deathsPerOneMillion = deathsPerOneMillion;
     }
 
     public String getCountry() {
         return country;
     }
 
-    public Integer getTotalCases() {
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public CountryInfo getCountryInfo() {
+        return countryInfo;
+    }
+
+    public void setCountryInfo(CountryInfo countryInfo) {
+        this.countryInfo = countryInfo;
+    }
+
+    public Integer getCases() {
         return cases;
+    }
+
+    public void setCases(Integer cases) {
+        this.cases = cases;
     }
 
     public Integer getTodayCases() {
         return todayCases;
     }
 
-    public Integer getTotalDeaths() {
+    public void setTodayCases(Integer todayCases) {
+        this.todayCases = todayCases;
+    }
+
+    public Integer getDeaths() {
         return deaths;
+    }
+
+    public void setDeaths(Integer deaths) {
+        this.deaths = deaths;
     }
 
     public Integer getTodayDeaths() {
         return todayDeaths;
     }
 
+    public void setTodayDeaths(Integer todayDeaths) {
+        this.todayDeaths = todayDeaths;
+    }
+
     public Integer getRecovered() {
         return recovered;
+    }
+
+    public void setRecovered(Integer recovered) {
+        this.recovered = recovered;
+    }
+
+    public Integer getActive() {
+        return active;
+    }
+
+    public void setActive(Integer active) {
+        this.active = active;
     }
 
     public Integer getCritical() {
         return critical;
     }
 
-    public Integer getCasesPerOneMillion() {
+    public void setCritical(Integer critical) {
+        this.critical = critical;
+    }
+
+    public float getCasesPerOneMillion() {
         return casesPerOneMillion;
     }
+
+    public void setCasesPerOneMillion(float casesPerOneMillion) {
+        this.casesPerOneMillion = casesPerOneMillion;
+    }
+
+    public float getDeathsPerOneMillion() {
+        return deathsPerOneMillion;
+    }
+
+    public void setDeathsPerOneMillion(float deathsPerOneMillion) {
+        this.deathsPerOneMillion = deathsPerOneMillion;
+    }
 }
-
-
